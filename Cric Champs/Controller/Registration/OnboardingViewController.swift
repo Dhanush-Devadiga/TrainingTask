8806e32c18b0621222465d45c1f69a94d84b3407
Dhanush-Devadiga <dhanush.devadiga@robosoftin.com>
2023-01-09 12:52:04 +0530

    status bar color commit

--- Cric Champs/Controller/Registration/OnboardingViewController.swift
+++ Cric Champs/Controller/Registration/OnboardingViewController.swift
@@ -44,7 +44,7 @@ class OnboardingViewController: UIViewController {
         super.viewDidLayoutSubviews()
         forwardButton.setUpButtonBackGround( colours: [ #colorLiteral(red: 1, green: 0.7294117647, blue: 0.5490196078, alpha: 1), #colorLiteral(red: 0.9960784314, green: 0.3607843137, blue: 0.4156862745, alpha: 1)], cornerRadius: 0)
         if view.bounds.width < view.bounds.height {
-            visualView.constant = view.bounds.height - 70
+            visualView.constant = view.bounds.height - 50
         }
     }
     
